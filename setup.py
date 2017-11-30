8a2ac1272c9e8b0058203a2010d61ea61cfe1b15
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-11-30 22:04:10 +0100

    Added first version of a minimal implementation.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tedega_examples',
+      version='0.1',
+      description='Examples of service implementations',
+      license='MIT',
+      packages=['tedega_examples'],
+      zip_safe=False)
