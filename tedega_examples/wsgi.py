1d3083c85a331114fcc9b89777aabccbeefc9048
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-12-10 20:10:26 +0100

    Added simple WSGI script.

---
+++ tedega_examples/wsgi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tedega_examples.app import build_app
+application = build_app("tedega_examples")
+
+if __name__ == "__main__":
+    application.run()
